7628edd0136ab02584bfb0d22c7ab6eb53e645f9
naing <naing@AUNGMYOSETT>
2024-06-26 20:32:22 +0800

    Add project files.

---
+++ Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "Win";
+            string guess = "";
+            int point = 0;
+            
+            while (guess != secretWord && point < 3)
+            {
+                Console.Write("Guess the word:");
+                guess = Console.ReadLine();
+                point++;
+            }
+            if (guess != secretWord)
+            { Console.WriteLine("You Lose!"); }
+            else { Console.WriteLine("You Win!"); }
+            
+            Console.ReadLine();
+        }
+    }
+}
